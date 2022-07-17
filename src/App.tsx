685fad221c414c85131613c019576e8881805131
Kyle Conrad <kwconrad@protonmail.com>
2022-07-17 09:42:33 -0700

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <h1 className="text-5xl text-slate-500">You are the prototype</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,7 @@
+export function Counter() {
+  return (
+    <div>
+      <h1 className="text-green-500">Counter feature</h1>
+    </div>
+  );
+}
